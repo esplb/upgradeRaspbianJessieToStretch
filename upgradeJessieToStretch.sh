2f8a6394cac0587d0e5a26496e5e2dcad5c2bc74
Jing-Heng-Zhang <214413073@qq.com>
2019-04-30 17:02:37 +0800

    Update upgradeJessieToStretch.sh

--- upgradeJessieToStretch.sh
+++ upgradeJessieToStretch.sh
@@ -6,3 +6,5 @@ sudo sed -i /deb/s/jessie/stretch/g /etc/apt/sources.list.d/*.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
